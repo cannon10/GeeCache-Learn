308826c69d64da765ea6b4baa0babdfd75ff1d12
叶泉威 <14295154+ye-quanwei@user.noreply.gitee.com>
2025-09-01 19:15:46 +0800

    day-3-http-server

---
+++ day3-http-server/geecache/http/http.go
@@ -0,0 +1,13 @@
+package geecache
+
+import (
+	"log"
+	"net/http"
+)
+
+type server int
+
+func (h *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
+	w.Write([]byte("hello world"))
+}
